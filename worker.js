c2f52bdc0cfd228d570027ec38f7d723bd79436a
SzymonNowak1 <szymon289@op.pl>
2020-04-27 13:24:39 +0200

    First One

---
+++ worker.js
@@ -0,0 +1,11 @@
+/* Function to open hidden nav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Function to close hidden nav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "white";
+}
